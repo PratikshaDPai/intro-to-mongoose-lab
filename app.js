2739fe987c3ba2e9733779c51808e1e4280543de
Pratiksha Pai <1312pratiksha@gmail.com>
2025-03-05 21:08:50 -0800

    Move Customer model to customer.js and connect to Mongo

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
+require("dotenv").config(); //include .env variables
+
 const prompt = require("prompt-sync")();
 const mongoose = require("mongoose");
+const Customer = require("./customer");
 
-const selection = prompt({
-  ask: `What would you like to do?
-  
-1. Create a customer
-2. View all customers
-3. Update a customer
-4. Delete a customer
-5. quit
+const uri = process.env.MONGODB_URI;
 
-Number of action to run: `,
-  value: "1",
-});
+async function run() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Success: connected to mongo");
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
 
-console.log(selection);
+  let isRunning = true;
+  while (isRunning) {
+    const selection = prompt({
+      ask: `What would you like to do?
+      
+    1. Create a customer
+    2. View all customers
+    3. Update a customer
+    4. Delete a customer
+    5. quit
+    
+    Number of action to run: `,
+      value: "1",
+    });
+
+    switch (selection) {
+      case "1": {
+        break;
+      }
+      case "5": {
+        isRunning = false;
+        break;
+      }
+    }
+  }
+
+  await mongoose.disconnect();
+}
+
+run();
